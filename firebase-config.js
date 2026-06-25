779dae1fe67b26a8ed82ef758e4f219088e505c3
sherona-appalo <amsheronaappalo@gmail.com>
2026-06-25 12:53:58 +0530

    Add Firebase configuration

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// ─── Replace these values with your Firebase project config ───────────────────
+// Firebase Console → Project Settings → Your Apps → SDK setup and configuration
+const firebaseConfig = {
+  apiKey:            "AIzaSyDdqyIqR-ng53NNByAkAHYvBd3bpi6PfXc",
+  authDomain:        "attendance-dashboard-76d72.firebaseapp.com",
+  projectId:         "attendance-dashboard-76d72",
+  storageBucket:     "attendance-dashboard-76d72.firebasestorage.app",
+  messagingSenderId: "1010921889239",
+  appId:             "1:1010921889239:web:858736562bd3eabc66ff92"
+};
